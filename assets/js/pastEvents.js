6c8413da25bd2376528673de4c6e301c9ae18ea4
edwingpe <edwing.machado1@gmail.com>
2023-03-08 05:43:56 -0300

    Solucionado tamanho imagenes

--- assets/js/pastEvents.js
+++ assets/js/pastEvents.js
@@ -11,20 +11,30 @@ function filterPastEvents (data){
 function createCards(events){
     let cards=''
     for (const info of events) {
-        cards += `
-        <div class="col">
-          <div class="card shadow">
-            <img src=${info.image}>
-            <div class="card-body d-flex flex-column justify-content-center">
+      cards +=`
+      <div class="col">
+        <div class="card shadow">
+          <img class="card-img" src=${info.image}>
+          <div class="card-body row g-0">
+            <div class="col-12">
               <h1 class="card-title">${info.name}</h1>
+            </div>
+            <div class="col-12">
               <p class="card-sub-title">${info.date}</p>
+            </div>
+            <div class="col-12">
               <p class="card-info">${info.description}</p>
-              <p class="card-sub-title">$${info.price}</p>
+            </div>
+            <div class="col-4">
+            <p class="card-sub-title">$${info.price}</p>
+            </div>
+            <div class="col-8">
               <a class="card-btn" href="./details.html" target="_self">More Info</a>
             </div>
           </div>
         </div>
-        `
+      </div>
+      `
     }
     return cards
 }
